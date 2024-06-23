11fc7f0547d6b6eeb596a171f682626d1520879e
Sai Lao Kham <saikham.email@gmail.com>
2024-06-23 21:29:33 +0700

    feat(01-api-setup): setup API

---
+++ src/api/news.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const BASE_URL = `https://newsapi.org/v2/everything`;
+
+export type SearchQuery = {
+  text: string;
+};
+
+export type NewsType = {
+  source: {id: null | string; name: string};
+  author: string | null;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string | null;
+  publishedAt: string;
+  content: string;
+};
+
+export const fetchNews = async ({text}: SearchQuery): Promise<NewsType[]> => {
+  try {
+    const fullURL = `${BASE_URL}?q=${text}&from=2024-06-23&sortBy=popularity&apiKey=2915770e2dd0433d8818397354a3d088`;
+    const response = await axios.get(fullURL);
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch news related to ' + text);
+  }
+};
